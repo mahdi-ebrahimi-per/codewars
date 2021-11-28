971e95d77e8a0ee7996a16bbd8d1f58bf53a7a12
mahdi-ebrahimi-per <76206312+mahdi-ebrahimi-per@users.noreply.github.com>
2021-11-28 09:20:59 +0330

    Update Extract the domain name from a URL.py

--- Extract the domain name from a URL/Extract the domain name from a URL.py
+++ Extract the domain name from a URL/Extract the domain name from a URL.py
@@ -3,15 +3,23 @@
 
 def domain_name(url):
     
+    # استاندارد کردن
+    # https://url  -> url  # or www.url
+    # http://url  -> url  # or www.url
     if 'http:' in url:
         url = url[7:]
 
     elif 'https:' in url:
         url = url[8:]
 
+
+    # اش را پاک کند www. داشت قسمت www اگر اولش
+    # url -> url.com
+    # www.url -> url.com
     if 'www.' in url:
         url = url[4:]
 
+    # url.com -> [url, com][0] = name
     return url.split(".")[0]
 
 
