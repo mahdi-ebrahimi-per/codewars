ff5bb323c432c892dbe8ce07257d1708e5f76b7e
mahdi-ebrahimi-per <76206312+mahdi-ebrahimi-per@users.noreply.github.com>
2021-12-17 00:16:51 +0330

    Create Count characters in your string -cleaned.py

---
+++ Count characters in your string/Count characters in your string -cleaned.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/python
+
+from collections import Counter
+
+def count(string):
+    return Counter(string)
+
+
+#_____
+
+from collections import Counter
+
+def count(string):
+    return Counter(string)
+
+
+print(count("aaab"))
