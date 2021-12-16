c6e758f74efe8a05af803658f0c17b5152d2c1ca
mahdi-ebrahimi-per <76206312+mahdi-ebrahimi-per@users.noreply.github.com>
2021-12-17 00:17:01 +0330

    Update IP Validation.py

--- IP Validation/IP Validation.py
+++ IP Validation/IP Validation.py
@@ -35,10 +35,24 @@ def is_valid_IP(strng):
             return False
 
 
-
 print(is_valid_IP('192.168.200')) #False
 print(is_valid_IP('192.168.200.300')) #False
 print(is_valid_IP('abc.def.ghi.jkl')) #False 
 print(is_valid_IP('')) #False
 
+#___
+import re
+def is_valid_IP(strng):
+    return bool(re.match(r'^((\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])(\.(?!$)|$)){4}(?=$)',strng))
+#___
+import socket
 
+def is_valid_IP(addr):
+    try:
+        socket.inet_pton(socket.AF_INET, addr)
+        return True
+    except socket.error:
+        return False
+#___
+def is_valid_IP(s):
+    return s.count('.')==3 and all(o.isdigit() and 0<=int(o)<=255 and str(int(o))==o for o in s.split('.'))
